b9297fdb78d01f83324ee21ce05d64ea54617691
Abel731 <abelp9289@gmail.com>
2024-08-18 22:10:22 -0400

    probando cosas en el archivo principal de la app dentro de la carpeta principal del proyecto que es llamado src y el archivo es el App.vue

--- proyecto-vue/src/App.vue
+++ proyecto-vue/src/App.vue
@@ -1,85 +1,28 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <!-- directiva de vue lladamo @click donde cuando se haga click en el boton se ejecuta
+   la ejecucion de lo que  se asigne adentro en este ejemplo la funcion incrementar -->
+  <button @click="incrementar">incrementar</button>
+  <button @click="decrementar">decrementar</button>
 </template>
 
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+//funcion para incrementar el valor utilizando value
+const incrementar = () => {
+  contador.value++;
+}
+const decrementar = () => {
+  contador.value--;
+}
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+/* el "contador" se pone rojo por que esta dentro de una etiqueta <p> */
+p {
+  color: red;
 }
 </style>
